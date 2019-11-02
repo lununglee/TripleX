b6df4f836b9516e268d22fa18f3012fd4072ad98
lununglee <leo.lunung.lee@gmail.com>
2019-11-01 17:58:22 -0700

    U04 - Fixing Const Values

--- srcs/triple_x.cpp
+++ srcs/triple_x.cpp
@@ -2,20 +2,19 @@
 
 int	main()
 {
-
 	// DECLARATION OF VARIABLES
-	const int	a;
-	const int	b;
-	const int	c;
-	const int	sum;
-	const int	product;
+	const int	a = 4;
+	const int	b = 3;
+	const int	c = 2;
+	const int	sum = a + b + c;
+	const int	product = a * b * c;
 
-	// VALUES
-	a = 4;
-	b = 3;
-	c = 2;
-	sum = a + b + c;
-	product = a * b * c;
+	// // VALUES
+	// a = 4;
+	// b = 3;
+	// c = 2;
+	// sum = a + b + c;
+	// product = a * b * c;
 
 	// MESSAGE
 	std::cout << "You have reached the end of a long hallway..." << std::endl;
