1fb64a750ef2331d143acb940aca445a518ff4c5
lununglee <leo.lunung.lee@gmail.com>
2019-11-05 09:14:52 -0800

    U09 - Modified Message

--- srcs/triple_x.cpp
+++ srcs/triple_x.cpp
@@ -21,33 +21,29 @@ int	main()
 	// product = a * b * c;
 
 	// MESSAGE
-	std::cout << "You have reached the end of a long hallway..." << std::endl;
-	std::cout << "There is only one door at the end of this hallway and on that door you see a numpad..." << std::endl;
-	std::cout << "You need to enter the correct codes to continue..." << std::endl << std::endl;
+	std::cout << "You're running away from Zombies and you've reached the end of a long hallway...\n";
+	std::cout << "There is only one door at the end of this hallway and on that door you see a numpad...\n";
+	std::cout << "You need to enter the correct codes to continue...\n\n";
 
 	// OUTPUT PROCESSED VALUE
-	std::cout << "There are a total of 3 numbers in the code" << std::endl;
-	std::cout << "The code adds up to: " << CodeSum << std::endl;
-	std::cout << "The code multiplies up to: " << CodeProduct << std::endl;
+	std::cout << "There are a total of 3 numbers in the code\n";
+	std::cout << "The code adds up to: \n" << CodeSum;
+	std::cout << "The code multiplies up to: \n" << CodeProduct;
 
 	// READING USER INPUT
-	std::cout << "Please enter 3 numbers delimited (separated) by spaces" << std::endl;
+	std::cout << "Please enter 3 numbers delimited (separated) by spaces\n";
 	std::cin >> GuessA >> GuessB >> GuessC;
-	std::cout << "Your guess was: " << GuessA << " | " << GuessB << " | " << GuessC << std::endl;
+	std::cout << "Your guess was: " << GuessA << " | " << GuessB << " | " << GuessC;
 
 	// PROCESSING USER INPUT
 	GuessSum = GuessA + GuessB + GuessC;
 	GuessProduct = GuessA * GuessB * GuessC;
 
+	// WIN CONDITION VERIFICATION
 	if (GuessSum == CodeSum && GuessProduct == CodeProduct)
-	{
-		std::cout << "You're in!" << std::endl;
-	}
+		std::cout << "You're in!\n";
 	else
-	{
-		std::cout << "You lost..." << std::endl;
-	}
-	
+		std::cout << "Your brains got eaten\n";
 
 	return (0);
 }
